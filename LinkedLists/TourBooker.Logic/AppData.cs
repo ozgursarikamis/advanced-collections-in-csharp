e841005730e43c9864f078e394742c08c93c6ace
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-24 01:52:32 +0300

    LL project added

---
+++ LinkedLists/TourBooker.Logic/AppData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TourBooker.Logic
+{
+	public class AppData
+	{
+		public List<Country> AllCountries { get; private set; }
+		public void Initialize(string csvFilePath)
+		{
+			CsvReader reader = new CsvReader(csvFilePath);
+			this.AllCountries = reader.ReadAllCountries().OrderBy(x => x.Name).ToList();
+		}
+	}
+}
---
+++ LinkedLists/TourBooker.UI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TourBooker.Logic;
+
+namespace TourBooker.UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private AppData AllData { get; } = new AppData();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			AllData.Initialize(@"G:\projects\Courses\Pluralsight\AdvancedCollections\LinkedLists\PopByLargest.csv");
+			this.DataContext = AllData;
+		}
+
+		private void tbxCountryCode_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			string code = tbxCountryCode.Text;
+			this.grdSelectCountry.DataContext = GetCountryWithCode(code);
+		}
+
+		private Country GetCountryWithCode(string code)
+		{
+			if (code.Length != 3)
+				return null;
+			return AllData.AllCountries.Find(x => x.Code == code);
+		}
+	}
+}
