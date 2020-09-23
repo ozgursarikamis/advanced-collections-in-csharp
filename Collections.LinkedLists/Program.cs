011e2c63b3ca9a9bb0140f8a02fe7cd4fac7bce8
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-24 02:34:17 +0300

    Linked list implementation

---
+++ Collections.LinkedLists/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections.LinkedLists
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            LinkedListNode<string> node = new LinkedListNode<string>("orange");
+            DisplayProperties(node);
+
+            LinkedList<string> ll = new LinkedList<string>();
+            ll.AddLast(node);
+            Console.WriteLine("After adding node to the empty LinkedList");
+            DisplayProperties(node);
+
+            ll.AddFirst("red");
+            ll.AddLast("yellow");
+
+            Console.WriteLine("After adding red and yellow");
+            DisplayProperties(node);
+
+            Console.ReadLine();
+        }
+
+        private static void DisplayProperties(LinkedListNode<string> node)
+        {
+            Console.WriteLine();
+            Console.WriteLine(node.List == null
+                ? "Node is not linked"
+                : $"Node belongs to a linked list with {node.List.Count} item(s)");
+
+            Console.WriteLine(node.Previous == null
+                ? "this is the first node"
+                : $"Value of previous node is {node.Previous.Value}");
+
+            Console.WriteLine($"Value of the current node: {node.Value}");
+
+            Console.WriteLine(node.Next == null 
+                ? "This is the last node"
+                : $"Value of next node: {node.Next.Value}");
+
+            Console.WriteLine();
+        }
+    }
+}
