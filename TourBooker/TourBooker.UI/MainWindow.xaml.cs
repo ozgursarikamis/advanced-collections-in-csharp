488ecb4642b6ae41bf27faf652fa756c2797a412
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-23 21:40:55 +0300

    project files added

---
+++ TourBooker/TourBooker.UI/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TourBooker.Logic;
+
+namespace TourBooker.UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private AppData AllData { get; } = new AppData();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			AllData.Initialize(@"G:\projects\Courses\Pluralsight\AdvancedCollections\TourBooker\PopByLargest.csv");
+			DataContext = AllData;
+		}
+
+		private void tbxCountryCode_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			var code = tbxCountryCode.Text;
+			grdSelectCountry.DataContext = GetCountryWithCode(code);
+		}
+
+		private Country GetCountryWithCode(string code)
+		{
+			return code.Length != 3 ? null : AllData.AllCountries.Find(x => x.Code == code);
+		}
+	}
+}
