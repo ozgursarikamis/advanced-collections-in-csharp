9de793a62ebd1191e7843c199885d40bcd59c9a5
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-24 03:19:21 +0300

    stack from array

--- Collections.Stacks/Program.cs
+++ Collections.Stacks/Program.cs
@@ -8,13 +8,22 @@ namespace Collections.Stacks
     {
         private static void Main()
         {
-            Stack<int> stack = new Stack<int>();
+            var stack = new Stack<int>();
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
             stack.Push(4);
 
             foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            int[] arr = new int[] {1, 2, 3, 4};
+            var stackFromArray = new Stack<int>(arr);
+
+            foreach (var item in stackFromArray)
             {
                 Console.WriteLine(item);
             }
