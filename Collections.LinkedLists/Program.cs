014afdff9ebe2a3d03cdd7add091fcb375f1fe39
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-24 03:01:57 +0300

    LinkedList with concrete class

--- Collections.LinkedLists/Program.cs
+++ Collections.LinkedLists/Program.cs
@@ -7,30 +7,41 @@ namespace Collections.LinkedLists
     {
         private static void Main()
         {
-            string[] students = { "Jenifer", "Angelina", "Vera" };
-            LinkedList<string> list = new LinkedList<string>(students);
-            IterateLists(list);
+            var person1 = new Person("Person 1", "Sarikamış", null);
+            var person2 = new Person("Person 2", "Sarikamış", person1);
+            var person3 = new Person("Person 3", "Sarikamış", person2);
 
-            list.AddFirst("Natalie");
-            IterateLists(list);
-
-            var jeniferNode = list.Find("Jenifer") ?? throw new InvalidOperationException();
-            list.AddAfter(jeniferNode, "(After Jenifer)");
-            IterateLists(list);
-
+            var personList = new List<Person> { person1, person2, person3 };
+            var personLinkedList = new LinkedList<Person>(personList);
+            IterateLists(personLinkedList);
             Console.ReadLine();
         }
 
-        private static void IterateLists(LinkedList<string> linkedList)
+        private static void IterateLists(IEnumerable<Person> linkedList)
         {
             Console.WriteLine();
 
             foreach (var item in linkedList)
             {
-                Console.WriteLine($"\t {item}");
+                Console.WriteLine($"\t {item.FirstName}");
             }
 
             Console.WriteLine();
+        }
+    }
+
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person PrevNode { get; set; }
+        public Person NextNode { get; set; }
+
+        public Person(string firstName, string lastName, Person prevNode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PrevNode = prevNode;
         }
     }
 }
