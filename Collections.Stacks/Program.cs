99dcf0a92c14571b7343776164aab4c602a79e63
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-24 03:17:26 +0300

    generating Stack<T> instance

---
+++ Collections.Stacks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections.Stacks
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
